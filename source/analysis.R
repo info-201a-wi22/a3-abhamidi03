abdb23a5a2b0c82fe1919913269daffd76743ad1
abhamidi03 <abhamidi@uw.edu>
2022-03-03 11:05:24 -0800

    create analysis.R file

---
+++ source/analysis.R
@@ -0,0 +1,8 @@
+# Author - Atreya Bhamidi
+# Date - March 3rd, 2022
+
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+incarceration_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
